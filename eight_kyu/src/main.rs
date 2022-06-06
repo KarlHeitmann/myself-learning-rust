39c2897f22eae003298d0d85a80f8173742de2da
Karl Heitmann <khf1988@gmail.com>
2022-06-05 21:59:48 -0400

    my solution to name_shuffle is done

--- eight_kyu/src/main.rs
+++ eight_kyu/src/main.rs
@@ -1,11 +1,24 @@
-fn name_shuffle(s: &str) {
+fn name_shuffle(s: &str) -> String {
+//fn name_shuffle(s: &str) -> &str {
+  /*
   // https://www.codewars.com/kata/559ac78160f0be07c200005a/train/rust
   println!("{}", s);
+  //let (name: &str, last: &str) = s.split_once(' ').unwrap();
+  let (name, last) = s.split_once(' ').unwrap();
+  //return concat!(last, name);
+  return last + name;
+  */
+  let (name, last) = s.split_once(' ').unwrap();
+  //return concat!(last, name);
+  return last.to_string() + " " + name;
 }
 
 fn main() {
-  name_shuffle("john McClane");
-  name_shuffle("Mary jeggins");
-  name_shuffle("tom jerry");
+  let t1 = name_shuffle("john McClane");
+  let t2 = name_shuffle("Mary jeggins");
+  let t3 = name_shuffle("tom jerry");
+  println!("{}", t1);
+  println!("{}", t2);
+  println!("{}", t3);
 }
 
