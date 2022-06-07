39f9e9ceb1e5e0d82c46ffb18525fe4e58ca8fbe
Karl Heitmann <khf1988@gmail.com>
2022-06-06 23:42:58 -0400

    added solution to plurals

--- eight_kyu/src/main.rs
+++ eight_kyu/src/main.rs
@@ -5,12 +5,13 @@ mod nameshuffler;
 mod fixme_replace_all_dots;
 mod remove_string_spaces;
 mod even_or_odd;
+mod plurals;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
     if args.len() == 1 {
-        even_or_odd::run();
+        plurals::run();
     } else {
         if args[1] == "name_shuffler" {
             nameshuffler::run();
@@ -18,6 +19,8 @@ fn main() {
             fixme_replace_all_dots::run();
         } else if args[1] == "remove_string_spaces" {
             remove_string_spaces::run();
+        } else if args[1] == "plurals" {
+            plurals::run();
         } else if args[1] == "even_or_odd" {
             even_or_odd::run();
         }
